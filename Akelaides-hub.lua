8d6716acd5a20516be846627eca7af9c9e7e4b39
Akelaides <calvienokhoe@gmail.com>
2024-12-15 19:30:03 +0700

    Fixing

--- Akelaides-hub.lua
+++ Akelaides-hub.lua
@@ -18,6 +18,32 @@ local Tabs = {
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
 
+--Locals
+
+local LocalPlayer = Players.LocalPlayer
+local LocalCharacter = LocalPlayer.Character
+local humanoidRootPart = LocalCharacter:FindFirstChild("HumanoidRootPart")
+local PlayerGUI = LocalPlayer:FindFirstChild("PlayerGui")
+
+-- Variables
+
+local autoShake = false
+local autoShakeDelay = 0.1
+local autoShakeMethod = "KeyCodeEvent"
+local autoShakeClickOffsetX = 0
+local autoShakeClickOffsetY = 0
+local autoReel = false
+local autoReelDelay = 2
+local autoCast = false
+local autoCastMode = "Legit"
+local autoCastDelay = 2
+local zoneCast = false
+local Zone = "Brine Pool"
+local Noclip = false
+local AntiDrown = false
+local Target
+local FreezeChar = false
+
 local Options = Fluent.Options
 
 -- Teleportation Dropdown
@@ -26,8 +52,7 @@ local TeleportDropdown = Tabs.Teleportation:AddDropdown("TeleportationDropdown",
     Values = {
         "Moosewood",
         "Forsaken",
-        "Ancient Isles",
-        "Roslit Bay"
+        "Ancient Isle"
     },
     Multi = false,
     Default = 1
@@ -51,10 +76,6 @@ TeleportDropdown:OnChanged(function(Value)
         local ancientIslesPosition = Vector3.new(6059, 195, 284)
         humanoidRootPart.CFrame = CFrame.new(ancientIslesPosition)
         print("Teleporting to Ancient Isles")
-    elseif Value == "Roslit Bay" then
-        local roslitPosition = Vector3.new(-1474, 133, 702)
-        humanoidRootPart.CFrame = CFrame.new(roslitPosition)
-        print("Teleporting to Roslit")
     end
 end)
 
@@ -76,14 +97,80 @@ local WalkSpeedInput = Tabs.Settings:AddInput("WalkSpeed", {
     end
 })
 
--- Example Button
-Tabs.Main:AddButton({
-    Title = "Test Button",
-    Description = "A test button to trigger actions",
-    Callback = function()
-        print("Test button clicked!")
+local Toggle = Tabs.Main:AddToggle("AutoReel", {Title = "Auto Reel", Default = false })
+
+    AutoReel:OnChanged(function()
+        autoreelandshakeConnection = PlayerGui.ChildAdded:Connect(function(GUI)
+            if GUI:IsA("ScreenGui") and GUI.Name == "shakeui" then
+                GUI.safezone.ChildAdded:Connect(function(child)
+                    if child:isA("ImageButton") and child.name == "button" then
+                        if autoShake == true then
+                            task.wait(autoShakeDelay)
+                            if child.visible == true then
+                                if autoShakeMethod == "ClickEvent" then
+                                    local pos = child.AbsolutePosition
+                                    local size = child.AbsoluteSize
+                                    VirtualInputManager:SendMouseButtonEvent(pos.X + size.X / 2, pos.Y + size.Y / 2,0, true, LocalPlayer, 0)
+                                    VirtualInputManager:SeondMouseButtonEvent(pos.X + size.X / 2, pos.Y + size.Y / 2, 0, false, LocalPlayer, 0)
+                                --[[elseif autoShakeMethod == "firesignal" then
+                                    firesignal(child.MouseButton1Click)]]
+                                elseif autoShakeMethod == "KeyCodeEvent" then
+                                    while WaitForSomeone(RenderStepped) do
+                                        if autoShake and GUI.safezone:FindFirstChild(child.Name) ~= nil then
+                                            task.wait()
+                                            pcall(function()
+                                                GuiService.SelectedObject = child
+                                                if GuiService.SelectedObject = child then
+                                                    VirtualInputManager:SendKeyEvent(true, Enum.Keycode.Return, false, game)
+                                                    VirtualInputManager:SendKeyEvent(false, Enum.Keycode.Return, false, game)
+                                                end
+                                        end)
+                                    else
+                                        GuiService.SelectedObject = nil
+                                        break
+                                    end
+                                end
+                        end
+                    end
+            end
+    end)
+    if Gui:IsA("ScreenGui") and GUI.Name == "reel" then
+        if autoReel and ReplicatedStorage:WaitForChild("events"):WaitForChild("reelfinished") ~= nil then
+            repeat task.wait(autoReelDelay) ReplicatedStorage.events.reelfinished:FireServer(100, false) until GUI == nil
+        end
     end
-})
+
+    autoCastConnection == LocalCharacter.ChildAdded:Connect(function(child)
+        if child:IsA("Tool") and child:FindFirstChild("events"):WaitForChild("cast") ~= nil and autoCast then
+            task.wait(autoCastDelay)
+            if autoCastMode == "Legit" then
+                VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, LocalPlayer, 0)
+                HumanoidRootPart.ChildAdded:Connect(function()
+                    if humanoidRootPart:FindFirstChild("power") ~= nil and HumanoidRootPart.power.powerbar.bar ~= nil then
+                        humanoidRootPart.power.powerbar.bar.Changed:Connect(function(property)
+                            if property == "Size" then
+                                if humanoidRootPart =.power.powerbar.bar.size == UDim2.new(1, 0, 1, 0) then
+                                    VirtualInputManager:SendMouseButtonEvent(0, 0, 0, false, LocalPlayer, 0)
+                                end
+                            end
+                        end)
+                    end
+                end)
+            elseif autoCastMode == "Rage" then
+                child.events.cast:FireServer(100)
+            end
+        end
+    end)
+
+    autoCastConnection2 = PlayerGUI.ChildRemoved:Connect(function(GUI)
+        local Tool = LocalCharacter:FindFirstChildOfClass("Tool")
+        if GUI.Name == "reel" and autoCast == true and Tool ~= nil and Tool:FindFirstChild("events"): WaitForChild("cast") ~= nil then
+            task.wait(autoCastDelay)
+            if autoCastMode == "Legit" hten
+            VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, LocalPlayer, 0)
+        end
+    end)
+    Options.MyToggle:SetValue(false)
 
 -- Additional Example Inputs and Features
 local Keybind = Tabs.Main:AddKeybind("Keybind", {
