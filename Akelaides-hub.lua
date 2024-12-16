3fbbb2e1ed5f3022974da9da17626f56d0a26455
Akelaides <calvienokhoe@gmail.com>
2024-12-17 05:53:58 +0700

    Trying to Fix

--- Akelaides-hub.lua
+++ Akelaides-hub.lua
@@ -26,7 +26,7 @@ Window.BackgroundTransparency = 0.5  -- Adjust transparency to enhance the acryl
 
 local Tabs = {
     Main = Window:AddTab({ Title = "Home", Icon = "home" }),
-    Autofarm = Window:AddTab({ Title = "Autofarm", Icon = "plane-takeoff"}),
+    Autofarm = Window:AddTab({ Title = "Autofarm", Icon = "plane"}),
     Teleportation = Window:AddTab({ Title = "Teleportation", Icon = "compass" }),
     Miscellaneous = Window:AddTab({ Title = "Miscellaneous", Icon = "boxes" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
